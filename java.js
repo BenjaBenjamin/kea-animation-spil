7f2a27d1e01c7779456124716366cfcc75570d99
Worm <Worm@Worms-MacBook-Air.local>
2018-11-02 08:28:19 +0100

    Point

--- java.js
+++ java.js
@@ -1,3 +1,5 @@
+let score = 0;
+
 window.addEventListener("load", sidenVises);
 
 function sidenVises() {
@@ -34,4 +36,32 @@ function startGame() {
 
     document.querySelector("#start").classList.remove("fade_out");
 
+    document.querySelector("#draabe").addEventListener("click", clickedbox1);
+    document.querySelector("#draabe1").addEventListener("click", clickedbox1);
+    document.querySelector("#sol").addEventListener("click", clickedbox1);
+    document.querySelector("#sol1").addEventListener("click", clickedbox1);
+    document.querySelector("#orm1").addEventListener("click", clickedbox2);
+    document.querySelector("#orm2").addEventListener("click", clickedbox2);
+    document.querySelector("#orm3").addEventListener("click", clickedbox2);
+    document.querySelector("#orm4").addEventListener("click", clickedbox2);
+    document.querySelector("#orm5").addEventListener("click", clickedbox2);
+    document.querySelector("#orm6").addEventListener("click", clickedbox2);
+}
+
+function clickedbox1() {
+    console.log("clickedbox1");
+    score = score + 10;
+    //score += 5;
+    //score++;
+    console.log(score);
+
+    document.querySelector("#score").innerHTML = ":  " + score;
+
+    console.log(this);
+    this.classList.add("hide");
+}
+
+function clickedbox2() {
+    console.log(this);
+    this.classList.add("hide");
 }
